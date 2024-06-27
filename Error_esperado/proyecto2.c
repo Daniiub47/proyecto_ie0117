8b80e21a36ef8e65f3dbecae0c129166c0c4ef46
Daniiub47 <daniiub47@gmail.com>
2024-06-26 21:12:50 -0600

    Subida de archivos para la parte 2

---
+++ Error_esperado/proyecto2.c
@@ -0,0 +1,32 @@
+/*
+********************************************************
+*** Universidad de Costa Rica.                       ***
+*** Programación Bajo Plataformas Abiertas - IE0117. ***
+*** Fecha: 27 de junio de 2024.                      ***
+*** Creadora: Daniela Ulloa Barboza. B77748.         ***
+********************************************************
+
+*** Proyecto: Error Esperado ***
+
+Descripción: Este código corresponde a la solución de la segunda parte del proyecto final, en este se obtiene
+un subdominio de similitud entre dos curvas cuadráticas, donde, tanto sus constantes, como el porcentaje de error,
+son definidos por el usuario, por lo tanto, se generan gráficas donde se visualizan las curvas y el rango de valores
+del eje X cartesiano, donde las curvas se distancien relativamente con un porcentaje de error menor al escogido por
+el usuario. */
+
+
+// Se importan las bibliotecas: 
+#include <stdio.h> // Esta es para el printf.
+#include <math.h> // Esta es para las operaciones matemáticas.
+#include <string.h> // Esta es para manejo de strings.
+#include <stdlib.h> // Se utiliza para la función system.
+
+// Definición de las funciones:
+
+// Código main:
+int main () {
+    
+    return 0;
+};
+
+// Bloque de funciones:
